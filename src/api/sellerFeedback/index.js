4cc5d5a011563b1fb81f970a56d9588339108708
Catriona Ferguson <114424301+cferg97@users.noreply.github.com>
2023-03-22 23:38:42 +0000

    Update index.js

--- src/api/sellerFeedback/index.js
+++ src/api/sellerFeedback/index.js
@@ -1,28 +1,45 @@
 import express from "express";
+import { activeCheckMiddleware } from "../../lib/auth/activeCheck.js";
+import { JWTAuthMiddleware } from "../../lib/auth/jwtAuth.js";
 import reviewModel from "./model.js";
 
 const reviewRouter = express.Router();
 
 reviewRouter.get("/:userid", async (req, res, next) => {
   try {
-    const reviews = await reviewModel.find({ sellerId: req.params.userid });
+    const reviews = await reviewModel
+      .find({ sellerId: req.params.userid })
+      .populate({
+        path: "sellerId",
+        select: "username -_id",
+      })
+      .populate({
+        path: "reviewerId",
+        select: "username -_id",
+      });
     res.send(reviews);
   } catch (err) {
     next(err);
   }
 });
 
-reviewRouter.post("/:userid", async (req, res, next) => {
-  try {
-    const newReview = new reviewModel({
-      ...req.body,
-      sellerId: req.params.userid,
-    });
-    await newReview.save();
-    res.status(204).send();
-  } catch (err) {
-    next(err);
+reviewRouter.post(
+  "/:userid",
+  JWTAuthMiddleware,
+  activeCheckMiddleware,
+  async (req, res, next) => {
+    try {
+      const newReview = new reviewModel({
+        ...req.body,
+        sellerId: req.params.userid,
+        reviewerId: req.user._id,
+      });
+      await newReview.save();
+      res.status(204).send();
+    } catch (err) {
+      next(err);
+    }
   }
-});
+);
 
 export default reviewRouter;
